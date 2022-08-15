d0aa63caabb140165650224ae07ed40959dc023a
tinshoo04 <mnovo04@gmail.com>
2022-08-15 09:22:59 -0300

    Se ha modificado los archivos relacionados con products.html para cumplir con las consignas. Se creó products.css.
    Se ha modificado los archivos relacionados con products.html para cumplir con las consignas. Se creó products.css.

---
+++ js/products.js
@@ -0,0 +1,31 @@
+const productsURL = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+function manageData() {
+    fetch(productsURL)
+    .then(response => response.json())
+    .then(data => {
+        const containerCategory = document.querySelector(".container-category")
+        let myProducts = data.products
+        for (let i = 0; i < myProducts.length; i++) {
+            let contentToAppend = `
+            <div id = car-info-container>
+
+            <img id = "car-pic" src = ${myProducts[i].image}>
+            
+            <h4 id = "title-car-info">${myProducts[i].name} - ${myProducts[i].cost} ${myProducts[i].currency}</h4>
+            <br>
+            <p> ${myProducts[i].description} </p>
+            <p> Este articulo ha sido comprado ${myProducts[i].soldCount} veces </p>
+            </div>
+            
+            `
+            containerCategory.innerHTML += contentToAppend
+        }
+        console.log(myProducts)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function(e){
+    manageData()
+})
+
