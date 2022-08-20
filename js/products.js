eb04d9862bf657c5169b8f10844cf44c8356c11e
tinshoo04 <mnovo04@gmail.com>
2022-08-19 21:56:52 -0300

    Se implementaron estilos de Bootstrap

--- js/products.js
+++ js/products.js
@@ -8,16 +8,20 @@ function manageData() {
         let myProducts = data.products;
         for (let i = 0; i < myProducts.length; i++) {
             let contentToAppend = `
-            <div id = car-info-container>
-
-            <img id = "car-pic" src = ${myProducts[i].image}>
-            
-            <h4 id = "title-car-info">${myProducts[i].name} - ${myProducts[i].cost} ${myProducts[i].currency}</h4>
-            <br>
-            <p id = "product-description">${myProducts[i].description}</p>
-            <p id = "product-count"> Este articulo ha sido comprado ${myProducts[i].soldCount} veces </p>
+            <div  class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${myProducts[i].image}" alt="Imagen de un ${myProducts[i].name}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${myProducts[i].name} - ${myProducts[i].cost} ${myProducts[i].currency}</h4>
+                            <small class="text-muted">Este articulo ha sido comprado ${myProducts[i].soldCount} veces</small>
+                        </div>
+                        <p class="mb-1">${myProducts[i].description}</p>
+                    </div>
+                </div>
             </div>
-            
             `
             containerCategory.innerHTML += contentToAppend;
         }
